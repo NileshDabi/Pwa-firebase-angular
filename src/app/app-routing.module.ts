e0ebd8ca0d47c267154405b06f19a1e169d2b1b1
nilesh <nileshpatel.np110@gmail.com>
2019-08-22 17:11:23 +0530

    initial commit for PWA-firebase

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {InterfaceComponent} from './interface/interface.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './admin/auth.guard';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'interface',component:InterfaceComponent,canActivate: [AuthGuard]},
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'reset-password',component:ResetPasswordComponent},
+  {path:'dashboard',component:DashboardComponent,canActivate: [AuthGuard]},
+  {path:'',redirectTo:'login',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxEchartsModule } from 'ngx-echarts';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InterfaceComponent } from './interface/interface.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbSpinnerModule, NbLayoutModule, NbCardModule,NbTabsetModule, 
+  NbListModule, NbUserModule, NbInputModule, NbToastrModule, NbTreeGridModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { EchartComponent } from './pages/dashboard/echart/echart.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InterfaceComponent,
+    RegisterComponent,
+    DashboardComponent,
+    LoginComponent,
+    EchartComponent,
+    ResetPasswordComponent
   ],
   imports: [
+    NbSpinnerModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbLayoutModule,
+    NbCardModule,
+    NbTabsetModule,
+    NbToastrModule.forRoot(),
+    NbListModule,
+    NbTreeGridModule,
+    NbInputModule,
+    NbEvaIconsModule,
+    NbUserModule,
+    NgxEchartsModule,
+    Ng2SmartTableModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
